df965b16d76b7f4445177feea28c93807160cbbc
wslrj <362606856@qq.com>
2018-12-06 10:39:23 +0800

    new

--- api/wxapp/wxapp.config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("APPID","wx80bb015d1ac34f04");
-define("SECRET","4867005d5c6b947d4355a2efa576e7af");
-?>
--- api/wxpay/lib/WxPay.Config.base.php
+++ api/wxpay/lib/WxPay.Config.base.php
@@ -5,13 +5,21 @@
 
 class WxPayConfig
 {
-	const APPID = 'wx06895c8a970b67f7';
-	const MCHID = '1501259351';
-	const KEY = 'nrt35i5w2vflbqjzklpnw3nmb24j0nve';
-	const APPSECRET = '47edffddca79782f2f5510bda16282b2';
-	const SSLCERT_PATH = '/var/www/html/bzmall/api/wxpay3/cert/apiclient_cert.pem';
-	const SSLKEY_PATH = '/var/www/html/bzmall/api/wxpay3/cert/apiclient_key.pem';
+	public static $APPID;
+	public static $MCHID;
+	public static $KEY;
+	public static $APPSECRET;
+	public static $SSLCERT_PATH;
+	public static $SSLKEY_PATH;
 	const CURL_PROXY_HOST = "0.0.0.0";//"10.152.18.220";
 	const CURL_PROXY_PORT = 0;//8080;
 	const REPORT_LEVENL = 1;
+	public static function init($wx){
+		self::$APPID = $wx['appid'];
+		self::$MCHID = $wx['mchid'];
+		self::$KEY = $wx['mchkey'];
+		self::$APPSECRET = $wx['appkey'];
+		self::$SSLCERT_PATH =ROOT_PATH.$wx['sslcert_path'];
+		self::$SSLKEY_PATH=ROOT_PATH.$wx['sslkey_path'];
+	}
 }
